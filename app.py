71c83e71a59a6a0f3f5e64f2a9dcff39bf65f532
Jahid Hasan Shuvo <jahid@pinesolutions.xyz>
2019-03-24 12:14:43 +0600

    Update completed:

--- app.py
+++ app.py
@@ -20,13 +20,6 @@ def index():
     students = cur.fetchall()
     return render_template('students/index.html',students=students)
 
-@app.route('/delete/<string:id>',methods=['GET'])
-def delete(id):
-    cur = mysql.connection.cursor()
-    cur.execute("delete from students where id=%s",(id,))
-    mysql.connection.commit()
-    return redirect(url_for('index'))
-
 @app.route('/insert',methods=['POST'])
 def insert():
     if request.method == "POST":
@@ -40,7 +33,25 @@ def insert():
         mysql.connection.commit()
         return redirect(url_for('index'))
 
+@app.route('/delete/<string:id>',methods=['GET'])
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute("delete from students where id=%s",(id,))
+    mysql.connection.commit()
+    return redirect(url_for('index'))
 
+
+@app.route('/update/<string:id>',methods=['POST'])
+def update(id):
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        cur = mysql.connection.cursor()
+        cur.execute("update students set name = %s,email = %s,phone = %s where id=%s",(name,email,phone,id))
+        mysql.connection.commit()
+        return redirect(url_for('index'))
 
 
 if __name__=='__main__':
