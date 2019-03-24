43469765e9623cf2edff85e1bdfee1851ed5115e
Jahid Hasan Shuvo <jahid@pinesolutions.xyz>
2019-03-24 09:58:21 +0600

    initialized and insert,select done

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template,redirect,request,url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'ok'
+app.config['MYSQL_DB'] = 'flask_app'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("select * from students")
+    students = cur.fetchall()
+    return render_template('students/index.html',students=students)
+
+@app.route('/delete')
+def delete(id):
+    cur = mysql.connection.cursor()
+    cur.execute("delete from students where id=%s",(id))
+
+@app.route('/insert',methods=['POST'])
+def insert():
+    if request.method == "POST":
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO students (name,email,phone) VALUES (%s, %s,%s)",(name,email,phone))
+
+        mysql.connection.commit()
+        return redirect(url_for('index'))
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
